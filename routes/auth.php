a612d116c05f7d9eb6e936d2d3a08dbf3703d6b1
Matthieu <matthieuquerel@gmail.com>
2026-02-18 14:54:48 +0100

    corection test

--- routes/auth.php
+++ routes/auth.php
@@ -10,11 +10,9 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('auth')->group(function () {
-
-    Route::middleware('guest')->group(function () {
-        Route::get('register', [RegisteredUserController::class, 'create'])
-            ->name('register');
+Route::middleware('guest')->group(function () {
+    Route::get('register', [RegisteredUserController::class, 'create'])
+        ->name('register');
 
     Route::post('register', [RegisteredUserController::class, 'store']);
 
@@ -56,5 +54,3 @@ Route::middleware('auth')->group(function () {
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
 });
-
-}); // end prefix('auth')
