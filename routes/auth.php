244694852d95d1c3f91fddb9300e8cb0409ddd5e
Matthieu <matthieuquerel@gmail.com>
2026-02-18 14:36:20 +0100

    update network

--- routes/auth.php
+++ routes/auth.php
@@ -10,9 +10,11 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
+Route::prefix('auth')->group(function () {
+
+    Route::middleware('guest')->group(function () {
+        Route::get('register', [RegisteredUserController::class, 'create'])
+            ->name('register');
 
     Route::post('register', [RegisteredUserController::class, 'store']);
 
@@ -54,3 +56,5 @@ Route::middleware('auth')->group(function () {
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
 });
+
+}); // end prefix('auth')
